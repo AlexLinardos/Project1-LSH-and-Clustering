64b7cda2424d30c4cd6b6edd59c1244bf8b2dfda
antonis-milios <antonis.milios@gmail.com>
2021-11-05 15:16:06 +0200

    fix seed

---
+++ LSH.hpp
@@ -0,0 +1,38 @@
+#include "hashing.hpp"
+
+class G
+{
+    int k; // number of H functions used in the algorithm
+    int tableSize;
+    int w; // windowSize inserted by the user
+    int d; // dimension of vector
+
+public:
+    G(int k, int tableSize, int window, int dimensions) : k(k), tableSize(tableSize), w(window), d(dimensions){};
+
+    int produce_g(Item p)
+    {
+        H test_H = H(w, d);
+        vector<int> h_vector = test_H.produce_k_h(p, 4);
+        for (int j = 0; j < 4; j++)
+        {
+            cout << h_vector[j] << ", ";
+        }
+        cout << endl;
+
+        default_random_engine eng{static_cast<long unsigned int>(time(0))}; // create a randomisation engine with time as seed
+
+        vector<float> r;
+        normal_distribution<float> nd(0, 1); // create a normal distribution N(0,1)
+
+        // create a vector v whose points follow the normal distribution
+        for (int i = 0; i < k; i++)
+        {
+            r.push_back(nd(eng));
+        }
+
+        long unsigned int m = (long unsigned int)(((long long)1 << 32) - (long long)5);
+        cout << m << endl;
+        return 0;
+    }
+};
--- hashing.hpp
+++ hashing.hpp
@@ -13,17 +13,20 @@ using namespace std;
 // hash function family H
 class H
 {
+    int k; // number of H functions used in the algorithm
     int w; // window
     int d; // vector dimensions
+    default_random_engine eng;
 
 public:
-    H(int window, int dimensions) : w(window), d(dimensions){};
+    H(int window, int dimensions) : w(window), d(dimensions), k(k)
+    {
+        unsigned int seed = chrono::system_clock::now().time_since_epoch().count();
+        default_random_engine eng(time(0)+clock()); // create a randomisation engine with time as seed
+    };
 
     int produce_h(Item p)
     {
-        unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-        default_random_engine eng(seed); // create a randomisation engine with time as seed
-
         vector<float> v;
         normal_distribution<float> nd(0, 1); // create a normal distribution N(0,1)
 
@@ -65,43 +68,6 @@ public:
         //     cout << h_vector[j] << ", ";
         // }
         return h_vector;
-    }
-};
-
-class G
-{
-    int k;
-    int tableSize;
-    int w; // window
-    int d; // vector dimensions
-
-public:
-    G(int k, int tableSize, int window, int dimensions) : k(k), tableSize(tableSize), w(window), d(dimensions){};
-
-    int produce_g(Item p)
-    {
-        H test_H = H(w, d);
-        vector<int> h_vector = test_H.produce_k_h(p, 4);
-        for (int j = 0; j < 4; j++)
-        {
-            cout << h_vector[j] << ", ";
-        }
-        cout << endl;
-
-        default_random_engine eng{static_cast<long unsigned int>(time(0))}; // create a randomisation engine with time as seed
-
-        vector<float> r;
-        normal_distribution<float> nd(0, 1); // create a normal distribution N(0,1)
-
-        // create a vector v whose points follow the normal distribution
-        for (int i = 0; i < k; i++)
-        {
-            r.push_back(nd(eng));
-        }
-
-        long unsigned int m = (long unsigned int)(((long long)1 << 32) - (long long)5);
-        cout << m << endl;
-        return 0;
     }
 };
 
